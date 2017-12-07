760b63ea6726ccef2254d3ae10b1fe7bddb8a74e
tinfelise <tinfelise@gmail.com>
2017-12-06 22:33:11 -0800

    Moved hardcoded spreadsheet data to spreadsheet_data.js

---
+++ spreadsheet_data.js
@@ -0,0 +1,109 @@
+var options_build = [
+	{
+		'tranche': 1,
+		'strike': 0,
+		'shares': 0.825,
+	},
+	{
+		'tranche': 2,
+		'strike': 2.67242,
+		'shares': 2.462434,
+	}
+];
+
+var share_count_build = {
+	'Basic Shares': 50.8951089662646,
+	'Pref-Shares': 20.4993953808417,
+	'IPO Shares': 14.3,
+	'Greenshoe Shares': 1.5
+};
+
+var postIPO_realizations = [
+	{
+		'event':'Sold @ IPO',
+		'date': '11/22/17',
+		'shares':  6.5,
+		'stock_price': 12*(1-.07)
+	}
+];
+
+var TB_preIPO_shares = 56.8170170175447;
+
+var revenues = {
+	'2018': 216.083759525813,
+	'2019': 270
+};
+var net_debt = -30.4295;
+
+var purchase = {
+	'event':'Purchase',
+	'date': '9/8/14',
+	'amount': -200
+}
+var preIPO_realizations = [
+	{
+		'event':'Transaction Fees',
+		'date': '11/12/14',
+		'amount': 0.030899
+	},
+	{
+		'event':'Transaction Fees',
+		'date': '1/15/15',
+		'amount': 0.125
+	},
+	{
+		'event':'Transaction Fees',
+		'date': '4/1/15',
+		'amount': 0.1875
+	},
+	{
+		'event':'Transaction Fees',
+		'date': '7/1/15',
+		'amount': 0.1875
+	},
+	{
+		'event':'Transaction Fees',
+		'date': '10/1/15',
+		'amount': 0.1875
+	},
+	{
+		'event':'Transaction Fees',
+		'date': '12/16/15',
+		'amount': 0.1875
+	},
+	{
+		'event':'Transaction Fees',
+		'date': '3/31/16',
+		'amount': 0.25
+	},
+	{
+		'event':'Transaction Fees',
+		'date': '6/17/16',
+		'amount': 0.25
+	},
+	{
+		'event':'Transaction Fees',
+		'date': '9/29/16',
+		'amount': 0.25
+	},
+	{
+		'event':'Transaction Fees',
+		'date': '12/30/16',
+		'amount': 0.25
+	},
+	{
+		'event':'Transaction Fees',
+		'date': '4/11/17',
+		'amount': 0.3125
+	},
+	{
+		'event':'Realization',
+		'date': '6/29/17',
+		'amount': 44.90071809
+	},
+	{
+		'event':'Transaction Fees',
+		'date': '7/6/17',
+		'amount': 0.3125
+	}
+];
