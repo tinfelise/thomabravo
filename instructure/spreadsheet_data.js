3c56b1b5ba8abd1be6c478505b5abe77597cf5bd
tinfelise <tinfelise@gmail.com>
2022-02-24 10:11:56 -0800

    Will's Instructure Update 2/23

--- instructure/spreadsheet_data.js
+++ instructure/spreadsheet_data.js
@@ -16,29 +16,29 @@ var options_build = [
 	{
 		'tranche': 1,
 		'strike': 0,
-		'shares': 1.648824+4.010648
+		'shares': 4.723
 	}
 ];
 
 var share_count_build = {
-	'Basic Shares': 126,
+	'Basic Shares': 140.740569-12.5,
 	'IPO Shares': 12.5,
 	'Greenshoe Shares': 0
 };
 
 var revenues = {
-	'2021': 386.757,
-	'2022': 427.978
+	'2022': 458.926,
+	'2023': 509.303
 };
 var ebitdas = {
-	'2021': 119.828,
-	'2022': 148.204
+	'2022': 164.534,
+	'2023': 188.142
 };
 var uFCF = {
-	'2021': 135.217,
-	'2022': 165.684
+	'2022': 179.214,
+	'2023': 214.854
 };
-var net_debt = 550.01+15-89.808;
+var net_debt = 500-164.928;
 
 var TB_preIPO_shares = 122.065804;
 var transactions = [
